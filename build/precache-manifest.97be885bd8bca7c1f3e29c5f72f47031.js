23f8ca6b7da3467a6f99185b092aa716a7decf5b
hassanaliman <59656914+hassanaliman@users.noreply.github.com>
2020-05-23 22:30:26 +0700

    depannya
    bagian depannya

---
+++ build/precache-manifest.97be885bd8bca7c1f3e29c5f72f47031.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8aedf65ad91ea9f89a7cc27c98a4687",
+    "url": "/mern-todo-app/index.html"
+  },
+  {
+    "revision": "a2f43907996905077157",
+    "url": "/mern-todo-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "65cdfb6ec24d72bf8984",
+    "url": "/mern-todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2f43907996905077157",
+    "url": "/mern-todo-app/static/js/2.ba54a450.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mern-todo-app/static/js/2.ba54a450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cdfb6ec24d72bf8984",
+    "url": "/mern-todo-app/static/js/main.a403f5dd.chunk.js"
+  },
+  {
+    "revision": "62c7ec41cca17c6311d8",
+    "url": "/mern-todo-app/static/js/runtime-main.e8344314.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
